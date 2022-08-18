7406c08c07c7c57af12cda3d1d95ec786f2e54bd
Dave Strider <speedymota1234@gmail.com>
2022-08-18 09:03:57 -0400

    third commit

---
+++ Starter/script.js
@@ -0,0 +1,18 @@
+
+function userFunction() {
+    var choice = prompt("Choose R for rock, P for paper, S for scissors");
+    choice.Toupperclass
+}
+    console.log(choice); //debugging
+
+
+
+// 3. Store array of numeric characters to a variable and log the individual elements to the console using for-loop
+
+// 4. Store array of all lowercase alphabets to a variable and log the individual elements to the console using for-loop
+
+
+// 5. Store array of all uppercase alphabets to a variable and log the individual elements to the console using for-loop
+
+
+// 6. Store array of special characters to a variable and log the individual elements to the console using for-loop
