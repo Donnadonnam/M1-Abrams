9e860bd31fa9763e6c4c12f2637fa6ab1f8d6078
Donnadonnam <123767522+Donnadonnam@users.noreply.github.com>
2023-05-25 12:46:49 -0500

    Donnadonnam/M1-Abrams

--- Starter/script.js
+++ Starter/script.js
@@ -1,14 +1,9 @@
-
-function userFunction() {
-    var choice = prompt("Choose R for rock, P for paper, S for scissors");
-    choice.Toupperclass
-}
-    console.log(choice); //debugging
-
-
-
-// 3. Store array of numeric characters to a variable and log the individual elements to the console using for-loop
-
+# 
+#     var choice = prompt("Choose R for rock, P for paper, S for scissors");
+# }
+# 
+# 
+# 
 // 4. Store array of all lowercase alphabets to a variable and log the individual elements to the console using for-loop
 
 
@@ -16,3 +11,4 @@ function userFunction() {
 
 
 // 6. Store array of special characters to a variable and log the individual elements to the console using for-loop
+# 
